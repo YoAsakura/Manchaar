2acec73b8027f9a8fc8b46e1a8ef74f8084abeb9
Roman Murunov <54361554+YoAsakura@users.noreply.github.com>
2021-05-10 23:30:15 +0300

    коммит

--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ const menuBtn = document.querySelector(".page-header__mobile-menu-open");
 const menu = document.querySelector(".page-header__nav");
 
 menuBtn.addEventListener('click', function() {
-    menu.classList.toggle('page-header__nav--animate');
+    menu.classList.toggle('page-header__nav--hidden');
+    menu.classList.toggle('page-header__nav--show');
     menuBtn.classList.toggle('page-header__mobile-menu-open--opened');
+
+    if (!menu.classList.contains('page-header__nav--show')) {
+        setTimeout(hidden , 800)
+    } else {menu.classList.remove('none')
+ }
+
 });
 
-
+function hidden() {
+    menu.classList.add('none')
+}
 
 /* Слайдер */
 
@@ -39,29 +48,5 @@ slidePrevBtn.addEventListener('click', function() {
         
     slideFirstId = slideFirstId - 1;
     } else {slideFirstId = slideMaxId};
-    slideArr[slideFirstId].classList.add('slider__item--active');
-});
-
-slidePrevBtn.addEventListener('touchstart', function(e) {
-    slideArr.forEach(function(el) {
-        el.classList.remove('slider__item--active');
-    });
-
-    if ((slideFirstId <= 0) == false) {
-        
-    slideFirstId = slideFirstId - 1;
-    } else {slideFirstId = slideMaxId};
-    slideArr[slideFirstId].classList.add('slider__item--active');
-});
-
-slideNextBtn.addEventListener('touchstart', function(e) {
-    slideArr.forEach(function(el) {
-        el.classList.remove('slider__item--active');
-    });
-
-    if ((slideFirstId >= slideMaxId) == false) {
-    slideFirstId = slideFirstId + 1;
-    } else {slideFirstId = 0};
-
     slideArr[slideFirstId].classList.add('slider__item--active');
 });
