9cd8e81c11e552ae2e149f8ff81f4752e4cc2da9
Roman Murunov <54361554+YoAsakura@users.noreply.github.com>
2021-05-11 12:57:22 +0300

    коммит

--- js/main.js
+++ js/main.js
@@ -50,3 +50,29 @@ slidePrevBtn.addEventListener('click', function() {
     } else {slideFirstId = slideMaxId};
     slideArr[slideFirstId].classList.add('slider__item--active');
 });
+
+/* Таймер */
+
+// Set the date we're counting down to
+var countDownDate = new Date("July 5, 2021 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + " : " + hours + " : "
+    + minutes + " : " + seconds;
+    
+}, 1000);
